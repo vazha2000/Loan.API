0c4dfcf2d7aedb8d951251624f24de2cef36f479
vazha2000 <vaja.siqtura2000@gmail.com>
2024-03-25 19:38:34 +0400

    configure files; add identity tables in sql server

---
+++ Loan.API/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Loan.API.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? UserName {  get; set; }
+        public int? Age { get; set; }
+        public decimal? Salary { get; set; }
+        public bool? IsBlocked { get; set; }
+    }
+}
