cd2264d33bebf412d21d36a8f5544c94a2826a2a
vazha2000 <vaja.siqtura2000@gmail.com>
2024-03-28 19:02:45 +0400

    add auth tests

---
+++ Loan.API.Tests/AuthControllerTests/LoginTests.cs
@@ -0,0 +1,73 @@
+﻿using Loan.API.Controllers;
+using Loan.API.Models.DTOs.Auth;
+using Loan.API.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loan.API.Tests.AuthControllerTests
+{
+    public class LoginTests
+    {
+        [Fact]
+        public async Task Login_Successful_ReturnsToken()
+        {
+            // Arrange
+            var loginDto = new LoginDto
+            {
+                UserName = "validUser",
+                Password = "validPassword"
+            };
+
+            var expectedToken = "mockToken";
+
+            var mockAuthService = new Mock<IAuthService>();
+            mockAuthService.Setup(service => service.LoginAsync(loginDto))
+                           .ReturnsAsync(expectedToken);
+
+            var controller = new AuthController(mockAuthService.Object);
+
+            // Act
+            var result = await controller.Login(loginDto);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var responseObject = okResult.Value;
+            var tokenProperty = responseObject.GetType().GetProperty("Token");
+            var token = tokenProperty.GetValue(responseObject).ToString();
+            Assert.Equal(expectedToken, token);
+        }
+
+
+
+        [Fact]
+        public async Task Login_IncorrectCredentials_ReturnsBadRequest()
+        {
+            // Arrange
+            var loginDto = new LoginDto
+            {
+                UserName = "invalidUser",
+                Password = "invalidPassword"
+            };
+
+            var mockAuthService = new Mock<IAuthService>();
+            mockAuthService.Setup(service => service.LoginAsync(loginDto))
+                           .ThrowsAsync(new ArgumentException("incorrect username of password"));
+
+            var controller = new AuthController(mockAuthService.Object);
+
+            // Act
+            var result = await controller.Login(loginDto);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("incorrect username of password", badRequestResult.Value);
+        }
+
+    }
+}
---
+++ Loan.API.Tests/AuthControllerTests/UserRegisterTests.cs
@@ -0,0 +1,76 @@
+﻿using Loan.API.Controllers;
+using Loan.API.Models.DTOs.Auth;
+using Loan.API.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loan.API.Tests.AuthControllerTests
+{
+    public class UserRegisterTests
+    {
+        [Fact]
+        public async Task UserRegister_SuccessfulRegistration_ReturnsOk()
+        {
+            // Arrange
+            var registerDto = new UserRegisterDto
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                UserName = "johndoe",
+                Email = "john@mail.com",
+                Age = 30,
+                Salary = 50000,
+                Password = "password"
+            };
+
+            var mockAuthService = new Mock<IAuthService>();
+            mockAuthService.Setup(service => service.UserRegisterAsync(registerDto))
+                           .Verifiable();
+
+            var controller = new AuthController(mockAuthService.Object);
+
+            // Act
+            var result = await controller.UserRegister(registerDto);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("User registered successfully", okResult.Value);
+            mockAuthService.Verify();
+        }
+
+        [Fact]
+        public async Task UserRegister_UserAlreadyExists_ReturnsConflict()
+        {
+            // Arrange
+            var registerDto = new UserRegisterDto
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                UserName = "existingUser",
+                Email = "john@mail.com",
+                Age = 30,
+                Salary = 50000,
+                Password = "password"
+            };
+
+            var mockAuthService = new Mock<IAuthService>();
+            mockAuthService.Setup(service => service.UserRegisterAsync(registerDto))
+                           .ThrowsAsync(new InvalidOperationException($"User with this User Name already exists"));
+
+            var controller = new AuthController(mockAuthService.Object);
+
+            // Act
+            var result = await controller.UserRegister(registerDto);
+
+            // Assert
+            var conflictResult = Assert.IsType<ConflictObjectResult>(result);
+            Assert.Equal("User with this User Name already exists", conflictResult.Value);
+        }
+
+    }
+}
